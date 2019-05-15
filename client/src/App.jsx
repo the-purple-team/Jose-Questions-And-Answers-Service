731292975adfa63b88cc03b34895a8bf194cf2d9
Jose Valadez <vinmajj@gmail.com>
2019-05-14 23:55:07 -0700

    Component render data from api response

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,17 +8,17 @@ class Questions extends React.Component {
     super(props);
     this.state = {
       test: 'What are you doing here?',
-      product: ''
+      product: {}
     }
   }
 
   componentDidMount() {
     let id = window.location.pathname
-    console.log(window.location.pathname, `CURRENT WINDOW PATH NAME`)
+    // console.log(window.location.pathname, `CURRENT WINDOW PATH NAME`)
       axios.get(`/products${id}`)
         .then(response => {
           console.log(response, `this is is going well`)
-          this.setState( {db: response} );
+          this.setState( {product: response.data} );
         })
         .catch(err => {
           console.log(err)
@@ -31,32 +31,38 @@ class Questions extends React.Component {
   }
 
   render() {
+    const {product} = this.state;
+    console.log(JSON.stringify(product) === '{}');
     return (
       <div>
         <hr className="a-divider-normal"/>
         <h2 className="a-color-base askWidgetHeader">Customer questions & answers</h2>
+
 				<div className="askWidgetQuestions askLiveSearchHide">
-          <div className="a-row a-spacing-small a-spacing-top-base">
-            <input className="askSearchForm" 
-            type="Text" 
-            placeholder="Enter A Product ID" 
-            value={this.state.product}
-            
-            />
+          { JSON.stringify(product) === '{}' ? (
+            <h3>Enter a product ID in url</h3>
+          ) : (
+            <div className="a-row a-spacing-small a-spacing-top-base">
+            {this.state.product.questions.map(questions => 
+              <div>
+                Question: {questions.question}
+                {questions.answers.map(answer =>
+                  <div>
+                  <div>Answer: {answer.answer}</div>
+                  <div>By {answer.user} on {moment(answer.createdAt).format('LL')}</div>
+                  </div>
+                )}
+              </div>
+            )}
           </div>
-					{this.props.dummy.data.questions.map(questions => 
-						<div>
-							Question: {questions.question}
-							{questions.answers.map(answer =>
-								<div>
-								<div>Answer: {answer.answer}</div>
-								<div>By {answer.user} on {moment(answer.createdAt).format('LL')}</div>
-								</div>
-							)}
-						</div>
-					)}
+            
+            )}
+
+          
 
 				</div>
+
+
 				<hr />
       </div>
     )
