0384967f8dcd5b1d0cfadb19ac32439d4b37e53b
Jose Valadez <vinmajj@gmail.com>
2019-05-30 22:01:03 -0700

    Added CSS styling

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -23,7 +23,7 @@ class Questions extends React.Component {
       axios.get(`/products${id}`)
         .then(response => {
           console.log(response, `this is is going well`)
-          this.setState( {product: response.data} );
+          this.setState({ product: response.data });
         })
         .catch(err => {
           console.log(err)
@@ -41,102 +41,143 @@ class Questions extends React.Component {
     axios.post(`/ask/vote/question/${question_id}`,
       {
         vote: voteValue,
-	      product: product_id
+        product: product_id
       })
-        .then((response) => {
-          const questionId = response.data.question_id;
-          const voteValue = response.data.votes;
-          const questions = [...this.state.product.questions];
-          // find the question_id and update the vote value
-          questions.forEach(question => {
-            if (question.question_id === questionId) {
-              question.votes = voteValue;
-            }
-          });
-          this.setState({ [questions]: questions});
+      .then((response) => {
+        const questionId = response.data.question_id;
+        const voteValue = response.data.votes;
+        const questions = [...this.state.product.questions];
+        // find the question_id and update the vote value
+        questions.forEach(question => {
+          if (question.question_id === questionId) {
+            question.votes = voteValue;
+          }
         });
+        this.setState({ [questions]: questions });
+      });
 
   }
 
   render() {
-    const {product} = this.state;
+    const { product } = this.state;
     // questios array
-    const  data = this.state.product.questions; 
+    const data = this.state.product.questions;
     return (
       <div>
         <div id="ask-btf_feature_div">
-          <div> </div>
-        
-        <hr className="a-divider-normal"/>
-        <h2 className="a-color-base askWidgetHeader">Customers questions & answers</h2>
-				<div className="askWidgetQuestions askLiveSearchHide">
-          <div>
-            <Search questions={data}/>
-            
-          </div>
-          { JSON.stringify(product) === '{}' ? (
-            <h3>Enter a product ID in url</h3>
-          ) : (
-            <div className="a-row a-spacing-small a-spacing-top-base" style={{textAlign: 'center'}}>
-            {this.state.product.questions.length === 0 ? (
-              <div className="askQuestionExample">
-                <div className="askTypicalExample">Typical questions asked about products:</div>
-                <div className="askExampleQuestion">&nbsp;-&nbsp; Is the item durable?</div>
-                <div className="askExampleQuestion">&nbsp;-&nbsp; Is this item easy to use?</div>
-                <div className="askExampleQuestion">&nbsp;-&nbsp; What are the dimensions of this item?</div>
-              </div>
-            ) : this.state.product.questions.map(questions => 
-              <div>
-                <div className="vote-count">
-                  <ul className="vote voteaxios">
-                    <li className="up-vote">
-                      <form className="up" onSubmit={this.changeVote}>
-                        <input name="voteValue" value="1" type="hidden"/>
-                        <input name="product_id" value={this.state.product._id} type="hidden"/>
-                        <input name="question_id" value={questions.question_id} type="hidden" />
-                        <input type="submit" value="vote up" className="arrow-up"/>
-                      </form>
-                    </li>
-                    <li className="vote-value">
-                      <span>{questions.votes}</span>
-                      <span>
-                        <br></br>
-                        votes
-                      </span>
-                    </li>
-                    <li className="down-vote">
-                      <form className="down" onSubmit={this.changeVote}>
-                        <input name="voteValue" value="-1" type="hidden"/>
-                        <input name="product_id" value={this.state.product._id} type="hidden"/>
-                        <input name="question_id" value={questions.question_id} type="hidden" />
-                        <input type="submit" value="vote down" className="arrow-down" />
-                      </form>
-                    </li>
-                  </ul>
-                </div>
-                <div className="">Question: {questions.question}</div>
-                {/* {if answers length is greater than 1, show first answer,
-                  create a clickable link 'see more answer (number of answer hidden),} */}
-                
-                {questions.answers.map(answer =>
-                  <div>
-                  <div key={answer._id}><b>Answer:</b> {answer.answer}</div>
-                  <div>By {answer.user} on {moment(answer.createdAt).format('LL')}</div>
+          <div id="Ask"></div>
+          <div id="ask_lazy_load_div">
+            <div className="askInlineWidget">
+              <hr className="a-divider-normal" />
+              <h2 className="a-color-base askWidgetHeader">Customers questions & answers</h2>
+              <div className="askWidgetQuestions askLiveSearchHide">
+                <div className="a-row a-spacing-small a-spacing-top-base">
+                  <div className="a-section askBtfSearchViewContent">
+                    <Search questions={data} />
                   </div>
-                )}
+                </div>
+                {JSON.stringify(product) === '{}' ? (
+                  <h3>Enter a product ID in url</h3>
+                ) : (
+                    <div className="a-section a-spacing-none askBtfTopQuestionsContainer" style={{ textAlign: 'center' }}>
+                      <span className="askTopQandA">
+                        <div className="a-section askTeaserQuestions">
+                          {this.state.product.questions.length === 0 ? (
+                            <div className="askQuestionExample">
+                              <div className="askTypicalExample">Typical questions asked about products:</div>
+                              <div className="askExampleQuestion">&nbsp;-&nbsp; Is the item durable?</div>
+                              <div className="askExampleQuestion">&nbsp;-&nbsp; Is this item easy to use?</div>
+                              <div className="askExampleQuestion">&nbsp;-&nbsp; What are the dimensions of this item?</div>
+                            </div>
+                          ) : this.state.product.questions.map(questions =>
+                            <div className="a-fixed-left-grid a-spacing-base">
+                              <div className="a-fixed-left-grid-inner" style={{ "paddingLeft": "65px" }}>
+                                <div className="a-fixed-left-grid-col a-col-left" style={{ "width": "65px", "marginLeft": "-65px", "float": "left" }}>
+                                  <ul className="vote voteaxios">
+                                    <li className="up-vote">
+                                      <form className="up" onSubmit={this.changeVote}>
+                                        <input name="voteValue" value="1" type="hidden" />
+                                        <input name="product_id" value={this.state.product._id} type="hidden" />
+                                        <input name="question_id" value={questions.question_id} type="hidden" />
+                                        <input type="submit" value="vote up" className="arrow-up" />
+                                      </form>
+                                    </li>
+                                    <li className="vote-value">
+                                      <span>{questions.votes}</span>
+                                      <span>
+                                        <br></br>
+                                        votes
+                                </span>
+                                    </li>
+                                    <li className="down-vote">
+                                      <form className="down" onSubmit={this.changeVote}>
+                                        <input name="voteValue" value="-1" type="hidden" />
+                                        <input name="product_id" value={this.state.product._id} type="hidden" />
+                                        <input name="question_id" value={questions.question_id} type="hidden" />
+                                        <input type="submit" value="vote down" className="arrow-down" />
+                                      </form>
+                                    </li>
+                                  </ul>
+                                </div>
+                                {/* end of votes div */}
+
+                                <div className="a-fixed-left-grid-col a-col-right" style={{ "paddingLeft": "1%", "float": "left" }}>
+
+                                  <div key={questions.question_id} className="a-fixed-left-grid a-spacing-small">
+                                    <div className="a-fixed-left-grid-inner" style={{ "paddingLeft": "100px" }}>
+                                      <div className="a-fixed-left-grid-col a-col-left" style={{ "width": "100px", "marginLeft": "-100px", "float": "left" }}>
+                                        <span className="a-text-bold">Question:</span>
+                                      </div>
+                                      <div className="a-fixed-left-grid-col a-col-right" style={{ "paddingLeft": "0%", "float": "left" }}>
+
+                                        <span className="a-declarative">{questions.question}</span>
+
+                                      </div>
+                                    </div>
+                                  </div>
+                                  {/* {questions.answers.map(answer =>
+                                      <div className="a-fixed-left-grid a-spacing-base">
+                                        <div key={answer._id}><b>Answer:</b> {answer.answer}</div>
+                                        <div>By {answer.user} on {moment(answer.createdAt).format('LL')}</div>
+
+                                      </div>
+                                    )} */}
+                                  {questions.answers.map(answer =>
+                                    <div className="a-fixed-left-grid a-spacing-base">
+                                      <div className="a-fixed-left-grid-inner" style={{ "paddingLeft": "100px" }}>
+
+
+                                        <div key={answer._id} className="a-fixed-left-grid-col a-col-left" style={{ "width": "100px", "marginLeft": "-100px", "float": "left" }}>
+                                          <span className="a-text-bold">Answer:</span>
+                                        </div>
+                                        <div className="a-fixed-left-grid-col a-col-right" style={{ "paddingLeft": "0%", "float": "left" }}>
+                                          <span>{answer.answer}</span>
+                                          <br />
+                                          <span>By {answer.user} on {moment(answer.createdAt).format('LL')}</span>
+                                        </div>
+                                      </div>
+                                      {/* <div>By {answer.user} on {moment(answer.createdAt).format('LL')}</div> */}
+
+                                    </div>
+                                  )}
+                                </div>
+                                {/* end of QnA div */}
+                              </div>
+                            </div>
+                          )}
+
+                        </div>
+                      </span>
+
+                    </div>
+
+                  )}
               </div>
-            )}
             </div>
-          )}
-
-          
-
-				</div>
-        
           </div>
-
-          <hr class="a-spacing-large a-divider-normal" />
         </div>
+        <hr className="a-spacing-large a-divider-normal" />
+      </div>
     )
   }
 }
--- client/src/components/Search.jsx
+++ client/src/components/Search.jsx
@@ -13,10 +13,10 @@ class Search extends Component {
 	// updates the search value
 	handleChange(e) {
 		let query = e.target.value;
-		this.setState({search: query});
+		this.setState({ search: query });
 		// ut
 		console.log(this.state.search, `line 18`)
-		const {questions} = this.props;
+		const { questions } = this.props;
 		let result = [...this.state.searchResult];
 		for (let i = 0; i < questions.length; i++) {
 			let question = questions[i].question.split(' ');
@@ -26,29 +26,29 @@ class Search extends Component {
 			if (question.includes(query)) {
 				// insert the question into the result's array
 				result.push(questions[i]);
-				console.log( questions,`line 26`)
+				console.log(questions, `line 26`)
 				console.log(query, `LINE 28`)
 			}
-			
+
 		}
 		if (result.length) {
-			this.setState({ searchResult: result});
+			this.setState({ searchResult: result });
 			console.log(this.state, `result of search query`);
 		}
 	}
 
 	// determine if the search query is found in the question string
-	
+
 	render() {
 		return (
-		<div className="a-search a-span12">
-			<i className="fa fa-search"></i>
-			<input type="search" maxLength="150" autoComplete="off" placeholder="Have a question? Search for answers"
-			name="askQuestionText" className="a-input-text a-span12"
-			value={this.state.search}
-			onChange={(e) => this.handleChange(e)}
-			/>
-		</div>
+			<div>
+				<i className="fa fa-search a-icon a-icon-search"></i>
+				<input type="search" maxLength="150" autoComplete="off" placeholder="Have a question? Search for answers"
+					name="askQuestionText" className="a-input-text a-span12"
+					value={this.state.search}
+					onChange={(e) => this.handleChange(e)}
+				/>
+			</div>
 		)
 	}
 }
